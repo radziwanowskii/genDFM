d2897d82b828c1c689bcb605081743aafef2e52e
radziwanowskii <p.radziwanowski@student.maastrichtuniversity.nl>
2024-12-17 11:22:54 +0100

    changed description of the dataset

--- R/data.R
+++ R/data.R
@@ -1,47 +1,47 @@
 #' MacroPL Dataset
 #'
-#' This dataset contains macroeconomic variables from Poland, collected from various official sources.
+#' This dataset contains macroeconomic variables from Poland, collected from the \url{https://stat.gov.pl} and \url{https://nbp.pl}
 #' Each variable has an abbreviation and specific transformation applied for time series analysis.
 #'
-#' @format A data frame with several columns:
+#' @format Time series object containing 36 monthly macroeconomic indicators from Poland from January 2002 to July 2024.
 #' \describe{
 #'   \item{Date}{Date of the observation.}
-#'   \item{SBR}{State budget revenues. Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{SBE}{State budget expenditures. Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{BSB}{Balance of the state budget. Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{ExC}{Exports of goods (current prices). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{ExP}{Exports of goods (constant prices). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{ImC}{Imports of goods (current prices). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{ImP}{Imports of goods (constant prices). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{Man}{Manufacturing. Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{Bld}{Building construction. Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{UnE}{Unemployment. Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{UnC}{Unemployment (comparison with the last year). No transformation. Source: \url{https://stat.gov.pl}}
-#'   \item{CPI}{Consumer price indices. Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PFB}{Prices (food and non-alcoholic beverages). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PAT}{Prices (alcoholic beverages and tobacco products). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PCF}{Prices (clothing and footwear). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PHE}{Prices (housing or home use and energy carriers). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PHH}{Prices (home furnishings and housekeeping). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PH}{Prices (health). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PT}{Prices (transportation). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PC}{Prices (communications). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PRC}{Prices (recreation and culture). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{PE}{Prices (education). Transform (difference). Source: \url{https://stat.gov.pl}}
-#'   \item{DPU}{Dwellings put into use. No transformation. Source: \url{https://stat.gov.pl}}
-#'   \item{TEx}{Transaction price indices of exports. No transformation. Source: \url{https://stat.gov.pl}}
-#'   \item{TIm}{Transaction price indices of imports. No transformation. Source: \url{https://stat.gov.pl}}
-#'   \item{ToT}{Terms of trade. No transformation. Source: \url{https://stat.gov.pl}}
-#'   \item{M0}{M0 money aggregate. Transform (difference). Source: \url{https://nbp.pl}}
-#'   \item{M1}{M1 money aggregate. Transform (difference). Source: \url{https://nbp.pl}}
-#'   \item{M2}{M2 money aggregate. Transform (difference). Source: \url{https://nbp.pl}}
-#'   \item{M3}{M3 money aggregate. Transform (difference). Source: \url{https://nbp.pl}}
-#'   \item{ERE}{Exchange rate Euro (end of the month). No transformation. Source: \url{https://nbp.pl}}
-#'   \item{ERA}{Exchange rate Euro (monthly average). No transformation. Source: \url{https://nbp.pl}}
-#'   \item{USE}{Exchange rate USD (end of the month). No transformation. Source: \url{https://nbp.pl}}
-#'   \item{USA}{Exchange rate USD (monthly average). No transformation. Source: \url{https://nbp.pl}}
-#'   \item{Ref}{Reference rate. No transformation. Source: \url{https://nbp.pl}}
-#'   \item{Lom}{Lombard rate. No transformation. Source: \url{https://nbp.pl}}
+#'   \item{SBR}{State budget revenues. Transform (difference). }
+#'   \item{SBE}{State budget expenditures. Transform (difference).}
+#'   \item{BSB}{Balance of the state budget. Transform (difference).}
+#'   \item{ExC}{Exports of goods (current prices). Transform (difference).}
+#'   \item{ExP}{Exports of goods (constant prices). Transform (difference).}
+#'   \item{ImC}{Imports of goods (current prices). Transform (difference).}
+#'   \item{ImP}{Imports of goods (constant prices). Transform (difference).}
+#'   \item{Man}{Manufacturing. Transform (difference).}
+#'   \item{Bld}{Building construction. Transform (difference).}
+#'   \item{UnE}{Unemployment. Transform (difference).}
+#'   \item{UnC}{Unemployment (comparison with the last year). No transformation.}
+#'   \item{CPI}{Consumer price indices. Transform (difference).}
+#'   \item{PFB}{Prices (food and non-alcoholic beverages). Transform (difference).}
+#'   \item{PAT}{Prices (alcoholic beverages and tobacco products). Transform (difference).}
+#'   \item{PCF}{Prices (clothing and footwear). Transform (difference).}
+#'   \item{PHE}{Prices (housing or home use and energy carriers). Transform (difference).}
+#'   \item{PHH}{Prices (home furnishings and housekeeping). Transform (difference).}
+#'   \item{PH}{Prices (health). Transform (difference).}
+#'   \item{PT}{Prices (transportation). Transform (difference).}
+#'   \item{PC}{Prices (communications). Transform (difference).}
+#'   \item{PRC}{Prices (recreation and culture). Transform (difference).}
+#'   \item{PE}{Prices (education). Transform (difference).}
+#'   \item{DPU}{Dwellings put into use. No transformation.}
+#'   \item{TEx}{Transaction price indices of exports. No transformation.}
+#'   \item{TIm}{Transaction price indices of imports. No transformation.}
+#'   \item{ToT}{Terms of trade. No transformation.}
+#'   \item{M0}{M0 money aggregate. Transform (difference).}
+#'   \item{M1}{M1 money aggregate. Transform (difference).}
+#'   \item{M2}{M2 money aggregate. Transform (difference).}
+#'   \item{M3}{M3 money aggregate. Transform (difference).}
+#'   \item{ERE}{Exchange rate Euro (end of the month). No transformation.}
+#'   \item{ERA}{Exchange rate Euro (monthly average). No transformation.}
+#'   \item{USE}{Exchange rate USD (end of the month). No transformation.}
+#'   \item{USA}{Exchange rate USD (monthly average). No transformation.}
+#'   \item{Ref}{Reference rate. No transformation.}
+#'   \item{Lom}{Lombard rate. No transformation.}
 #' }
 #'
 #' @source \url{https://stat.gov.pl}, \url{https://nbp.pl}
