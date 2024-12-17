a17889b31a12632173b541092f951656d540879c
radziwanowskii <p.radziwanowski@student.maastrichtuniversity.nl>
2024-12-17 11:25:06 +0100

    changed the plot_IRF function to a method plot.irf, added checks of the data formats

--- R/favar.R
+++ R/favar.R
@@ -10,7 +10,7 @@ NULL
 #' @param data A data frame or matrix of data to be analyzed.
 #' @param r Number of static factors. Default is 3.
 #' @param n_lags Number of lags in the VAR model. Default is 13.
-#' @param slow_indices Indices of slow-moving variables. Default is NULL.
+#' @param slow_indices Indices of slow-moving variables. 
 #' @param direct_indices Indices of directly observed variables.
 #' @return A list containing the FAVAR model components:
 #' \itemize{
@@ -23,25 +23,40 @@ NULL
 #' Bernanke, B. S., Boivin, J., & Eliasz, P. (2005, 02). Measuring the Effects of Monetary Policy: A Factor-Augmented Vector Autoregressive (FAVAR) Approach*. The Quarterly Journal of Economics, 120(1), 387-422
 #' @name FAVAR
 #' @export
-FAVAR <- function(data, r = 3, n_lags = 13, slow_indices = NULL, direct_indices){
+FAVAR <- function(data, r = 3, n_lags = 13, slow_indices = NULL, direct_indices = NULL){
+  # Check if the data contains missing values
+  if(sum(is.na(data)) > 0){
+    stop("Data contains missing values")
+  }
   # Convert the data to a matrix
-  data_matrix <- as.matrix(data)
-  # Create matrix of direct variables
-  data_direct <- as.matrix(data[,direct_indices])
-
+  if(!is.matrix(data)){
+    data <- as.matrix(data)
+  }
+  if (!is.numeric(data)) {
+    stop("Data must be numeric")
+  }
   # Check if slow indices are provided
-  if(is.null(slow_indices)){
+  if(is.null(slow_indices) || length(slow_indices) < 2){
+    stop("Slow indices must be provided")
   }
-  else{
-    data_slow <- as.matrix(data[,slow_indices])
-    favar <- favar_prep_arma(data_matrix,r,n_lags,data_slow,data_direct)
+   # Check if direct indices are provided
+  if(is.null(direct_indices) || length(direct_indices) < 2){
+    stop("Direct indices must be provided")
   }
+
+  # Create matrices of direct and slow variables
+  data_direct <- data[,direct_indices]
+  data_slow <- data[,slow_indices]
+  
+  # Run the C++ code
+  favar <- favar_prep_arma(data,r,n_lags,data_slow,data_direct)
+  
   # Name the columns of the data
   # Assign names to the data_for_var matrix
    
   factor_names <- paste0("factor ", 1:r)
   if(is.numeric(direct_indices)){
-  direct_var_names <- colnames(data_matrix)[direct_indices]
+  direct_var_names <- colnames(data)[direct_indices]
   }
   else{
   direct_var_names <- direct_indices
@@ -49,7 +64,7 @@ FAVAR <- function(data, r = 3, n_lags = 13, slow_indices = NULL, direct_indices)
   colnames(favar[[1]]) <- c(factor_names, direct_var_names)
   colnames(favar[[2]]) <- c(factor_names, direct_var_names)
   colnames(favar[[3]]) <- factor_names
-  colnames(favar[[4]]) <- colnames(data_matrix)
+  colnames(favar[[4]]) <- colnames(data)
   favar.out <- list(data_for_var = favar[[1]], var = favar[[2]], factors = favar[[3]], loadings = favar[[4]])
   class(favar.out) <- "FAVAR"
   return(favar.out)
@@ -65,13 +80,13 @@ FAVAR <- function(data, r = 3, n_lags = 13, slow_indices = NULL, direct_indices)
 #' @param n_ahead Number of periods ahead to compute the IRF. Default is 49.
 #' @param n_boot Number of bootstrap samples. Default is 500.
 #' @param cumulative Whether to compute cumulative IRFs. Default is FALSE.
-#' @param shock The type of shock ("unit" or "sd"). Default is "unit".
+#' @param shock The type of shock used in the model. "unit" implements a one-unit shock to the schock variable, "sd" implements a shock of one standard deviation. Default is "unit".
 #' @param plot Whether to plot the IRF. Default is TRUE.
-#' @param alpha Significance level for the confidence intervals. Default is 0.05.
+#' @param alpha Significance level for the confidence intervals in the plot. Default is 0.05.
 #' @return A list containing the IRF and bootstrapped IRF:
 #' \itemize{
 #'   \item \code{irf}: The impulse response function.
-#'   \item \code{boot}: The bootstrapped impulse response function.
+#'   \item \code{boot}: The bootstrapped values of the impulse response function.
 #' }
 #' @references
 #' Bernanke, B. S., Boivin, J., & Eliasz, P. (2005, 02). Measuring the Effects of Monetary Policy: A Factor-Augmented Vector Autoregressive (FAVAR) Approach*. The Quarterly Journal of Economics, 120(1), 387-422
@@ -80,6 +95,10 @@ FAVAR <- function(data, r = 3, n_lags = 13, slow_indices = NULL, direct_indices)
 favar_IRF <- function(favar, shock_variable, response_variable = NULL,
 n_ahead = 49, n_boot = 500, cumulative = FALSE, shock = "unit", plot = TRUE, alpha = 0.05){
   
+  if(class(favar) != "FAVAR"){
+    stop("Input must be of class FAVAR")
+  }
+
   boot_data <- favar[[1]]
   VAR <- favar[[2]]
   loadings <- favar[[4]]
@@ -137,13 +156,14 @@ n_ahead = 49, n_boot = 500, cumulative = FALSE, shock = "unit", plot = TRUE, alp
     irf_result <- all_irf[,response_variable_index]
     boot_result <- boot_irf[,response_variable_index,]
   }
-  if(plot == TRUE){
-    plot_IRF(irf_result,boot_result,alpha)
-  }
- 
   # Return the IRF and the bootstrapped IRF
 irf.out <- list(irf = irf_result, boot = boot_result)
 class(irf.out) <- "IRF"
+
+if(plot == TRUE){
+    #plot.irf(irf_result,boot_result,alpha)
+    plot.irf(irf.out,alpha)
+  }
 
 return(irf.out)
 }
@@ -152,27 +172,29 @@ return(irf.out)
 #'
 #' This function plots the Impulse Response Functions (IRFs) along with their confidence intervals.
 #'
-#' @param irf The impulse response function.
-#' @param boot_irf The bootstrapped impulse response function.
-#' @param alpha Significance level for the confidence intervals. Default is 0.05.
+#' @param x An object of class "irf".
+#' @param alpha Significance level for the confidence intervals in the plot. Default is 0.05.
 #' @return A plot of the IRFs with confidence intervals.
-#' @name plot_IRF
+#' @name plot.irf
 #' @export
-plot_IRF <- function(irf, boot_irf, alpha = 0.05) {
+plot.irf <- function(x, alpha = 0.05) {
+  irf <- x[[1]]
+  boot_irf <- x[[2]]
+  
   n_ahead <- length(irf)
-  quantiles <- row_quantiles(boot_irf, c(alpha, 1-alpha))
-    
-  p <- ggplot(data = data.frame(x = 1:n_ahead, lower = quantiles[, 1], upper = quantiles[, 2], irf = irf)) +
-    geom_ribbon(aes(x = x, ymin = lower, ymax = upper), fill = "#64b675", alpha = 0.5) +
-    geom_line(aes(x = x, y = irf), color = "#0b6306", linewidth = 1.5) +
-    geom_hline(yintercept = 0, linetype = "twodash", color = "black", size = 1) +
-    theme(
-      #panel.background = element_rect(fill = "#d3d3d3"),  # Darker background for the plot panel
-     # plot.background = element_rect(fill = "#a9a9a9"),   # Darker background for the entire plot
-      axis.title = element_text(size = 14, face = "bold", color = "black"),  # Customize axis titles
-      axis.text = element_text(size = 12, color = "black")  # Customize axis text
-    ) +
-    labs(x = "Horizon", y = "Y Axis Label")  # Add axis labels
-
-    print(p)  # Print the plot
+  quantiles <- row_quantiles(boot_irf, c(alpha, 1 - alpha))
+  
+  # Create the plot
+  plot(1:n_ahead, irf, type = "l", col = "#0b6306", lwd = 1.5, ylim = range(c(quantiles, irf)),
+       xlab = "Horizon", ylab = "Response", main = "Impulse Response Functions")
+  
+  # Add the confidence intervals
+  polygon(c(1:n_ahead, rev(1:n_ahead)), c(quantiles[, 1], rev(quantiles[, 2])),
+          col = rgb(100, 182, 117, maxColorValue = 255, alpha = 127), border = NA)
+  
+  # Add the IRF line again to ensure it is on top
+  lines(1:n_ahead, irf, col = "#0b6306", lwd = 1.5)
+  
+  # Add a horizontal line at y = 0
+  abline(h = 0, lty = "twodash", col = "black", lwd = 1)
 }
